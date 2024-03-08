57216f9276142870c9b03dfd20fd052dc0cd01a2
Oleksandr <infoformiti@gmail.com>
2024-03-08 12:54:36 +0000

    added changing a language

--- java.js
+++ java.js
@@ -4,3 +4,22 @@ onload = () => {
         clearTimeout(c);
     }, 1000);
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    const textElements = document.querySelectorAll('.animated_march_text');
+    const texts = [
+        "С Международным Женским Днем!", // Russian
+        "З Міжнародним жіночим днем!",     // Ukrainian
+        "Happy International Women's Day!" // English
+    ];
+    let currentIndex = 0;
+
+    function changeText() {
+        textElements.forEach((element, index) => {
+            element.textContent = texts[(currentIndex + index) % texts.length];
+        });
+        currentIndex = (currentIndex + 1) % texts.length;
+    }
+
+    setInterval(changeText, 4000);
+});
